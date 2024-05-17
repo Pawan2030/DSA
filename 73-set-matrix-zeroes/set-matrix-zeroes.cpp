5ea8fb081c17ede02189d67b9e5c6f347fc13957
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-05-18 01:57:02 +0530

    Time: 16 ms (10.65%) | Memory: 16.4 MB (41.51%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,49 +1,33 @@
 class Solution {
 public:
-    
     int row;
     int col;
 
-    void make_row_col_zero(vector<vector<int>>& matrix , int i , int j){
-
-        //--->>NEW LOGIC
-
-        for(int r = 0; r<row; r++){
-            matrix[r][j] = 0;
-        }
-
-        for(int c = 0; c<col; c++){
-            matrix[i][c] = 0;
-        }
-
-        return;
-    }
-
-    void setZeroes(vector<vector<int>>& matrix){
+    void setZeroes(vector<vector<int>>& matrix) {
 
         row = matrix.size();
         col = matrix[0].size();
 
-        vector<pair<int,int>> vec;
+        vector<int> row_zero(row , 0);
+        vector<int> col_zero(col , 0);
 
-        for(int i=0 ; i<row; i++){
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
 
-            for(int j=0; j<col; j++){
-
-                 if(matrix[i][j] == 0){
-                   vec.push_back({i , j});
-                 }
+                if (matrix[i][j] == 0) {
+                    row_zero[i] = 1;
+                    col_zero[j] = 1;
+                }
             }
-        } 
-
-        for(int i=0; i<vec.size(); i++){
-
-            int r = vec[i].first;
-            int c = vec[i].second;
-
-            make_row_col_zero(matrix , r , c);
         }
 
-        return;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                 
+                 if(row_zero[i] == 1 || col_zero[j] == 1){
+                    matrix[i][j] = 0;
+                 }
+            }
+        }
     }
 };
