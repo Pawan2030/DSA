b948c75012d3e8fef00568387fb7349cab34ac7b
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-05-27 15:55:49 +0530

    Time: 218 ms (57.93%) | Memory: 17.5 MB (81.97%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    
-    int MOD = 1000000007;
-    int t[100001][2][3]; //n , ab , cl
 
+    int t[100001][2][3];
 
-    int solve(int n , int ab , int cl){
-         
-         //purning
-        if(ab > 1 || cl > 2){
-            return 0;
-         }
+    #define MOD 1000000007
+
+    int solve(int n , int absent , int cl){
 
         //base case
-        if(n == 0){
+
+        if(absent >= 2 || cl >= 3){
+            return 0;
+        }
+
+        if( n == 0){
             return 1;
         }
 
-        if(t[n][ab][cl] != -1){
-            return t[n][ab][cl];
+        if(t[n][absent][cl] != -1){
+            return t[n][absent][cl];
         }
 
-        int A = solve(n-1 , ab+1 , 0) %MOD;
-        int L = solve(n-1 , ab ,  cl+1) %MOD;
-        int P = solve(n-1 , ab , 0) %MOD;
+        int P = solve(n-1 , absent   , 0) % MOD ;
+        int A = solve(n-1 , absent+1 , 0) % MOD ;
+        int L = solve(n-1 , absent   , cl+1) % MOD ;
 
-        return t[n][ab][cl] = ((A+L)%MOD + (P%MOD))%MOD;
+        return t[n][absent][cl] = ( (P + L) % MOD  +  A) % MOD ;
+
     }
 
     int checkRecord(int n) {
-
-        int ab = 0;
-        int cl = 0;
-
+        
         memset(t , -1 , sizeof(t));
-
-        return solve(n , ab , cl);
+        // n  Absent , cl
+        return solve(n , 0 , 0);
         
     }
-};
+}; 
