3b66820c8f985460240732b68f88cd51a4d3ce2f
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-07-14 19:26:50 +0530

    Time: 308 ms (7.02%) | Memory: 18.3 MB (73.53%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,49 +1,39 @@
 class Solution {
 public:
 
-   string removeString(string s , string str , int point , int &res){
+   void removeString(string &s , string str , int point , int &res){
 
-       stack<char> st;
-       int i = 0;
+       int i = 0; //place the char
+       int j = 0; //read the char
 
-       while(i < s.length()){
+       while(j < s.length()){
+
+          s[i] = s[j];
+          i++;
            
-           if(!st.empty() && st.top() == str[0] && s[i] == str[1]){
-               res += point;
-               st.pop();
-           }
-           else{
-             st.push(s[i]);
-           }
-           i++;
+          if(i >= 2 && s[i-2] == str[0] && s[i-1] == str[1]){
+              
+              res += point;
+              cout<<"I ->"<<i<<endl;
+              i   -= 2;
+          }
+           j++;
        }
-
-       //return rest string 
-       string temp = "";
-
-       while(!st.empty()){
-          temp += st.top();
-          st.pop();
-       }
-
-       reverse(temp.begin() , temp.end());
-       return temp;
+        s.resize(i);
    }
 
-
     int maximumGain(string s, int x, int y) {
+        
+        string max_point_str = x > y ? "ab" : "ba";
+        string min_point_str = x > y ? "ba" : "ab";
 
+        int max_point = max(x , y);
+        int min_point = min(x , y);
+        int res = 0;
 
-       string max_point_str = x > y ? "ab" : "ba";
-       string min_point_str = x > y ? "ba" : "ab";
+        removeString(s , max_point_str , max_point , res);
+        removeString(s , min_point_str , min_point , res);
 
-       int max_point = max(x , y);
-       int min_point = min(x , y);
-       int res = 0;
-
-       string temp = removeString(s   , max_point_str , max_point , res);
-       string tem = removeString(temp , min_point_str , min_point , res);
-
-       return res;
+        return res;
     }
 };
