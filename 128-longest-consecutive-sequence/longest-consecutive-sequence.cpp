273d113381327d49458b7d557a44c9e4320f1911
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-07-22 23:30:54 +0530

    Time: 61 ms (93.90%) | Memory: 49.8 MB (76.40%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -8,24 +8,22 @@ public:
         
         sort(begin(nums) , end(nums));
 
-        int mini = nums[0] - 1;
+        int mini = INT_MIN;
         int high = 0;
-        int cnt = 0; 
+        int cnt = 1; 
 
         for(int i = 0; i<nums.size(); i++){
 
             if(nums[i]-1 == mini){
                 mini = nums[i];
                 cnt++;
-                high = max(high , cnt);
             }
-            else if(nums[i] == mini){
-                continue;
-            }
-            else{
+            else if(nums[i] != mini){
                 cnt = 1;
-                mini = nums[i];
+                mini = nums[i];;
             }
+            
+            high = max(high , cnt);
         }
         return high;
     }
