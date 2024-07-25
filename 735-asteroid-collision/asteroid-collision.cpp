a3141518ec51dd9f7555f869c048c2e321da945c
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-07-25 18:35:06 +0530

    Time: 14 ms (19.51%) | Memory: 20.8 MB (30.16%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,60 +1,47 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-        
 
-         stack<int> st;
-         vector<int> healths = asteroids;
-         int n = asteroids.size();
+    vector<int> asteroidCollision(vector<int>& ast) {
 
-        for(int idx = 0; idx<n; idx++){
-             
-            int val = asteroids[idx];
+        stack<int> st;
+        int n = ast.size();
 
-            if(val > 0){
-                st.push(idx);
+        for(int i = 0; i<n; i++){
+
+            if(ast[i] > 0){
+                st.push(i);
             }
             else{
-               while(!st.empty() && asteroids[st.top()] > 0){
-                
-                int top = st.top();
-                st.pop();
-                
-                if(healths[top] > abs(healths[idx])){
-                    healths[idx] = 0;
-                    st.push(top);
-                    break;
-                }
-                else if(healths[top] < abs(healths[idx])){
-                    healths[top] = 0;
-                }
-                else{
-                    healths[top] = 0;
-                    healths[idx] = 0;
-                    break;
+
+                while(!st.empty() && ast[st.top()] > 0){
+
+                    if(ast[st.top()] > abs(ast[i])){
+                        ast[i] = 0;
+                        break;
+                    }
+                    else if(ast[st.top()] < abs(ast[i])){
+                        ast[st.top()]  = 0;
+                        st.pop();
+                    }
+                    else{
+                         ast[st.top()]  = 0; 
+                         ast[i] = 0;
+                         st.pop();
+                         break;
+                    }
                 }
             }
-          }  
+
         }
 
-        vector<int> arr;
+        vector<int> ans;
 
-        // while(!st.empty()){
-
-        //     int val = healths[st.top()];
-        //     st.pop();
-        //     arr.push_back(val);
-        // }
-
-        // reverse(arr.begin() , arr.end());
-
-        for(int i = 0 ; i<healths.size() ; i++){
-
-            if(healths[i] != 0){
-                arr.push_back(healths[i]);
+        for(int num : ast){
+            if(num != 0){
+                ans.push_back(num);
             }
         }
-
-        return arr;
+        
+        return ans;
     }
 };
