600770fa827f47718904db587ed1d2ed90191183
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-07-27 20:23:55 +0530

    Time: 20 ms (31.00%) | Memory: 18.5 MB (12.71%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    
+    void Dijsktra(int s , int k , unordered_map<int , vector<vector<int>>>& adj , vector<int>& miniCost){
+        
+        //priority_queue<pair<int,pair<int,int>> , vector<pair<int,pair<int,int>>> , greater<pair<int,pair<int,int>>>> pq;
+        queue<pair<int,pair<int,int>>> pq;
+        pq.push({0 , {0 , s}}); //cost -> stop, station
+      
+
+        while(!pq.empty()){
+            
+            int w    = pq.front().first;
+            int stop = pq.front().second.first;
+            int u    = pq.front().second.second;
+            pq.pop();
+
+            if(stop > k+1) continue;
+
+            for(auto &ngr : adj[u]){
+
+            int node = ngr[0];
+            int    d = ngr[1];
+
+            if(w+d < miniCost[node] && stop <= k){
+                miniCost[node] = w+d;
+                pq.push({w+d , {stop+1 , node}});
+            }
+        }
+
+    }
+}
+
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+        vector<int> miniCost(n , INT_MAX);
+        miniCost[src] = 0;
+
+        unordered_map<int , vector<vector<int>>> adj;
+
+        for(auto &fly : flights){
+
+            int u = fly[0];
+            int v = fly[1];
+            int w = fly[2];
+
+            adj[u].push_back({v , w});
+        }
+
+        Dijsktra(src , k , adj , miniCost);
+
+        for(int i =0; i<n; i++){
+            cout<<i<<"-> "<<miniCost[i]<<endl;
+        }
+
+        return miniCost[dst] != INT_MAX ? miniCost[dst] : -1;
+
+    }
+};
