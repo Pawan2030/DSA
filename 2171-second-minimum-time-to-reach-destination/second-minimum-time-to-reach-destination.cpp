2012638adb06bd93dfc94f8e5253386b25bf9551
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-07-28 23:52:26 +0530

    Time: 837 ms (5.22%) | Memory: 317.7 MB (5.21%) - LeetSync

---
+++ 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+    int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
+
+        vector<int> mini1(n+1 , INT_MAX);
+        vector<int> mini2(n+1 , INT_MAX);
+        mini1[1] = 0;
+        
+        unordered_map<int , vector<vector<int>>> adj;  // u -> v => w;
+        
+        for(auto edge : edges){
+
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back({v , time});
+            adj[v].push_back({u , time});
+        }
+
+        priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>> q;
+        q.push({0 , 1}); // {time , node}
+       
+        while(!q.empty()){
+
+            auto top = q.top();
+            int    w = top.first;
+            int    u = top.second;
+            q.pop();
+ 
+            int mode = w/change;
+
+            if(mode%2 == 1){
+                w = (mode+1)*change;
+            }
+
+            for(auto &it : adj[u]){
+
+                int v = it[0];
+                int d = it[1];
+               
+                if(w + d < mini1[v]){
+                    mini2[v] = mini1[v];
+                    mini1[v] = w+d;
+                    q.push({w+d , v});
+                }
+                else if(w + d > mini1[v] && w+d < mini2[v]){
+                    mini2[v] = w+d;
+                    q.push({w+d , v});
+                }
+            }
+            
+        }
+        return mini2[n];  
+    }
+};
