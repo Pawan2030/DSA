9218ceebe02d9534420ea1e54bb564d8ef5843a9
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-07-29 23:06:32 +0530

    Time: 486 ms (57.62%) | Memory: 200.1 MB (55.16%) - LeetSync

--- 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.cpp
+++ 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.cpp
@@ -2,57 +2,57 @@ class Solution {
 public:
 
     int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
+        
+        vector<int> d1(n+1 , INT_MAX);
+        vector<int> d2(n+1 , INT_MAX);
+        d1[1] = 0;
 
-        vector<int> mini1(n+1 , INT_MAX);
-        vector<int> mini2(n+1 , INT_MAX);
-        mini1[1] = 0;
-        
-        unordered_map<int , vector<vector<int>>> adj;  // u -> v => w;
-        
-        for(auto edge : edges){
+        unordered_map<int , vector<int>> adj;
+
+        for(auto &edge : edges){
 
             int u = edge[0];
             int v = edge[1];
-            adj[u].push_back({v , time});
-            adj[v].push_back({u , time});
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
 
-        //priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>> q;
-        queue<pair<int,int>> q;
-        q.push({0 , 1}); // {time , node}
-       
+        queue<pair<int , int>> q;
+        q.push({0,1});
+        int level = 0;
+
         while(!q.empty()){
 
-            auto top = q.front();
-            int    w = top.first;
-            int    u = top.second;
-            q.pop();
+            int size = q.size();
 
-           // if(u == n && mini2[n] != INT_MAX) return mini2[n];
- 
-            int mode = w/change;
+            while(size--){
 
-            if(mode%2 == 1){
-                w = (mode+1)*change;
-            }
+                int it = q.front().second;
+                int w  = q.front().first;
+                q.pop();
 
-            for(auto &it : adj[u]){
+                int div = w/change;
 
-                int v = it[0];
-                int d = it[1];
-               
-                if(w + d < mini1[v]){
-                    mini2[v] = mini1[v];
-                    mini1[v] = w+d;
-                    q.push({w+d , v});
+                if(div%2 == 1){
+                    w = (div+1)*change;
                 }
-                else if(w + d > mini1[v] && w+d < mini2[v]){
-                    mini2[v] = w+d;
-                    q.push({w+d , v});
+
+                for(auto ngr : adj[it]){
+
+                    if(time + w < d1[ngr]){
+                        d1[ngr] = time+w;
+                        q.push({time+w , ngr});
+                    }
+                    else if(time + w > d1[ngr] && time + w < d2[ngr]){
+                        d2[ngr] = time + w;
+                        q.push({time+w , ngr});
+                    }
                 }
             }
-            
+
+            level++;
         }
-        return mini2[n];  
+
+        return d2[n];
     }
 };
