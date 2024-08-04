fbdb3b29400989d787b254bfca9abbd987ca0eaa
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-08-04 23:37:17 +0530

    Time: 76 ms (81.80%) | Memory: 11.5 MB (77.98%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
-
     int mod = 1e9+7;
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        
-        int size = ( n * (n + 1)) / 2 ;
-        vector<int> sum(size , 0);
-        //int n = nums.size();
-         //sum[0] = nums[0];
 
-        int k = 0;
+        int cnt = 1;
+        int sum = 0;
+
+        priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>> pq;
 
         for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
 
-                if(i != j){
-                    sum[k] = sum[k-1] + nums[j];
-                    //cout<<sum[k]<<" , ";
-                }
-                else{
-                    sum[k] = nums[j];
-                }
-                k++;
-            }
-        }
+            pq.push({nums[i] , i});
 
-        sort(begin(sum) , end(sum));
-
-        for(int i=0; i<size; i++){
-           cout<<sum[i]<<" , ";
         }
         
-        int ans = 0;
-        for(int i=left; i<=right; i++){
-            ans = ( ans%mod + sum[i-1])%mod;
+        while(!pq.empty()){
+
+            int val = pq.top().first;
+            int idx = pq.top().second;
+            pq.pop();
+        
+            if(cnt >= left && cnt <= right){
+                sum = (sum%mod+ val%mod)%mod;
+            }
+
+            if(idx + 1 < n){
+                val += nums[idx+1];
+                pq.push({val , idx+1});
+            }
+
+            if(cnt > right){
+                break;
+            }
+
+            cnt++;
         }
-        return ans;
+    
+        return sum;
     }
 };
