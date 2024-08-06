a23e2530a8281786c20522e1f2e967004c6278a7
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-08-06 12:40:23 +0530

    Time: 195 ms (5.10%) | Memory: 27 MB (40.29%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -4,26 +4,31 @@ public:
 
     int minimumPushes(string word) {
          
-         unordered_map<char , int> freq;
+         unordered_map<char , int> freq; //o(n)
 
-         for(auto ch : word){
+         for(auto ch : word){ //0(n)
             freq[ch]++;
          }
 
-         vector<int> vec;
+         vector<int> vec;//o(n)
 
-         for(auto it : freq){
+         for(auto it : freq){ //o(n)
 
             int f = it.second;
             vec.push_back(f);
          }
 
-         sort(rbegin(vec) , rend(vec));
+         auto comp = [&](int a , int b){
+            cout<<a<<" "<<b<<endl;
+            return a > b;
+         };
+
+         sort(begin(vec) , end(vec) , comp); //o(nlogn)
 
          //int mul = 1;
          int miniTyped = 0;
 
-         for(int i=0; i<vec.size(); i++){
+         for(int i=0; i<vec.size(); i++){ //o(n)
               
              int mul = i/8 + 1;
 
