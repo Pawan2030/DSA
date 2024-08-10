3dfa0748101506f8ecaa9f6664368b0c07fd0f03
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-08-10 13:04:27 +0530

    Time: 3 ms (57.72%) | Memory: 12.2 MB (19.51%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.cpp
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -1,43 +1,70 @@
 class Solution {
-    bool isValid(int i, int j, vector<vector<int>>& grid) {
-        vector<int> count(10, 0);
-        for (int x = 0; x < 3; ++x) {
-            for (int y = 0; y < 3; ++y) {
-                int num = grid[i + x][j + y];
-                if (num < 1 || num > 9 || count[num] > 0) return false;
-                count[num]++;
-            }
-        }
-
-        int sum = grid[i][j] + grid[i][j+1] + grid[i][j+2];
-        
-        for(int x = 0; x < 3; ++x) {
-            if(sum != (grid[i + x][j] + grid[i + x][j + 1] + grid[i + x][j + 2])) return false;
-        }
-        
-        for(int y = 0; y < 3; ++y) {
-            if(sum != (grid[i][j + y] + grid[i + 1][j + y] + grid[i + 2][j + y])) return false;
-        }
-        
-        if(sum != (grid[i][j] + grid[i+1][j+1] + grid[i+2][j+2])) return false;
-        if(sum != (grid[i+2][j] + grid[i+1][j+1] + grid[i][j+2])) return false;
-
-        return true;
-    }
-    
 public:
-    int numMagicSquaresInside(vector<vector<int>>& grid) {
-        int cnt = 0;
-        int row = grid.size();
-        int col = grid[0].size();
-        
-        if(row < 3 || col < 3) return 0;
+    
+    int rows;
+    int cols;
 
-        for(int i = 0; i <= row - 3; ++i) {
-            for(int j = 0; j <= col - 3; ++j) {
-                if(isValid(i, j, grid)) cnt++;
+    bool isMagicalGrid(vector<vector<int>>& mat , int x , int y){
+        
+        //distinct and 1 to 9
+
+        unordered_set<int> st;
+
+        for(int i=0; i<3; i++){
+
+            for(int j=0; j<3; j++){
+
+                if(mat[x+i][y+j] <= 0 || mat[x+i][y+j] > 9 || st.count(mat[x+i][y+j])){
+                    return false;
+                }
+                st.insert(mat[x+i][y+j]);
             }
         }
-        return cnt;
+
+        //row and col for grid 3*3
+
+        int equal = mat[x][y] + mat[x][y+1] + mat[x][y+2];
+
+          for(int i=0; i<3; i++){
+             
+             if(equal != mat[x+i][y] + mat[x+i][y+1] + mat[x+i][y+2]){
+                return false;
+             }
+
+             if(equal != mat[x][y+i] + mat[x+1][y+i] + mat[x+2][y+i]){
+                return false;
+             }
+         }
+
+         //daigonal
+
+         if(equal != mat[x][y] + mat[x+1][y+1] + mat[x+2][y+2]){
+            return false;
+         }
+
+         if(equal != mat[x][y+2] + mat[x+1][y+1] + mat[x+2][y]){
+            return false;
+         }
+
+         return true;
+    }
+
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+
+       rows = grid.size();
+       cols = grid[0].size();
+
+       int magic = 0;
+
+       for(int i=0; i<=rows-3; i++){
+          
+          for(int j=0; j<=cols-3; j++){
+             
+              if(isMagicalGrid(grid , i , j)){
+                magic++;
+              }
+          }
+       } 
+       return magic;
     }
 };
