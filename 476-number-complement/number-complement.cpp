f3b8a054157d8343db0b292695c5b617cb6889ec
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-08-23 01:28:47 +0530

    Time: 3 ms (41.26%) | Memory: 7.6 MB (8.45%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
 
-   /*
-
-   int mask = same size of num bit size
-
-    int size = 
-
-    int mask = (1<<size) - 1;
-
-    return num ^ mask;
-
-   */
+   
 
     int findComplement(int num) {
 
         // num = 5;
         //xor with 1 with ever char
 
-        int num_bits = (int)(log2(num)) + 1;
+        // int num_bits = (int)(log2(num)) + 1;
 
-        for(int i=0; i<num_bits; i++){
+        // for(int i=0; i<num_bits; i++){
              
-            num = num^(1<<i);
-        }
+        //     num = num^(1<<i);
+        // }
         
-        return num;
+        // return num;
+
+        
+
+    //int mask = same size of num bit size
+
+    int size = (int)(log2(num)) + 1;
+
+    unsigned int mask = (1U<<size) - 1;
+
+    return num ^ mask;
+
+
     }
 };
