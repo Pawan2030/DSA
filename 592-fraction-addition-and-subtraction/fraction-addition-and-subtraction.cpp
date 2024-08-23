604f099c83d8670b53ca37574f942b062fc80b80
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-08-24 02:25:34 +0530

    Time: 3 ms (48.90%) | Memory: 7.7 MB (71.69%) - LeetSync

---
+++ 592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+
+   int gcd(int a , int b){
+      
+      int div = 0;
+
+      for(int i = 1; i<=min(a , b); i++){
+          
+          if(a%i == 0 && b%i == 0){
+              div = i;
+          }
+      }
+
+      return div;
+   }
+
+    string fractionAddition(string exp) {
+
+        int up = 0;
+        int dn = 1;
+        int i = 0;
+        int size = exp.length();
+
+
+       
+        while(i < size){
+
+           bool isNeg = false;
+           int upper = 0 , neeche = 0;
+
+
+            if(exp[i] == '-' || exp[i] == '+'){
+                if(exp[i] == '-') isNeg = true;
+                i++;
+            }
+
+            while(i < size && isdigit(exp[i])){
+
+                int val = exp[i] - '0'; 
+                upper = (upper*10) + val;
+                i++;
+            }
+
+            if(exp[i] == '/'){
+                i++;
+            }
+
+            while(i < size && isdigit(exp[i])){
+
+                int val = exp[i] - '0'; 
+                neeche = (neeche*10) + val;
+                i++;
+            }
+
+            if(isNeg){
+                upper *= -1;
+            }
+
+            up = (up*neeche) + (dn*upper);
+            dn = (dn * neeche);
+  
+        }
+
+        int div = abs(__gcd(up , dn));
+
+        // if(div == 0){
+        //     string res = to_string(up) + "/" + to_string(dn);
+        //     return res;
+        // }
+
+        up /= div;
+        dn /= div;
+
+        string ans = to_string(up) + "/" + to_string(dn);
+        return ans;
+    }
+};
