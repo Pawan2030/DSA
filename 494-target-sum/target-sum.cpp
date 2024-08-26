30a7c1d2283d830280bc6d403e2d5ad97e1db26e
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-08-27 02:06:15 +0530

    Time: 490 ms (21.31%) | Memory: 10.8 MB (91.14%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,7 +1,31 @@
 class Solution {
 public:
     
-    int solve(int i , vector<int>& nums , int n , int sum){
+   // vector<vector<int> dp;
+
+    // int solve1(int i , vector<int>& nums , int n , int sum){
+
+    //     if(i>=n){
+    //         if(sum == 0){
+    //           return 1;
+    //         } 
+    //         return 0;
+    //     }
+          
+    //     int incl = 0;
+    //     int excl = 0;
+
+    //     if(nums[i] <= sum){
+    //     incl = solve1(i+1 , nums , n , sum-nums[i]) + solve(i+1 , nums , n , sum);
+    //     }
+    //     else{
+    //         excl = solve1(i+1 , nums , n , sum);
+    //     }
+
+    //     return incl+excl;
+    // }
+
+    int solve2(int i , vector<int>& nums , int n , int sum){
 
         if(i>=n){
             if(sum == 0){
@@ -10,18 +34,12 @@ public:
             return 0;
         }
           
-        int incl = 0;
-        int excl = 0;
-
-        if(nums[i] <= sum){
-          incl = solve(i+1 , nums , n , sum-nums[i]) + solve(i+1 , nums , n , sum);
-        }
-        else{
-            excl = solve(i+1 , nums , n , sum);
-        }
-
+        int incl = solve2(i+1 , nums , n , sum-nums[i]);
+        int excl  = solve2(i+1 , nums , n , sum);
+        
         return incl+excl;
     }
+
 
     int findTargetSumWays(vector<int>& nums, int target) {
         
@@ -34,6 +52,6 @@ public:
 
         int sum = (total+target)/2;
 
-        return solve(0 , nums , n , sum);
+        return solve2(0 , nums , n , sum);
     }
 };
