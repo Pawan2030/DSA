e1ad8d57c1fcbdb08397e4755babed354064096e
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-08-28 01:03:43 +0530

    Time: 2 ms (87.47%) | Memory: 11.6 MB (58.00%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> res;
     int n;
 
-    void solve(vector<int>& nums, vector<int> temp, unordered_set<int> st) {
+    void solve(vector<int>& temp,  unordered_map<int,int>& mp) {
 
         // base case
         if (temp.size() == n) {
@@ -12,14 +12,14 @@ public:
         }
 
         // logic
-        for (int i = 0; i < n; i++) {
-
-            if (st.find(i) == st.end()) {
-                temp.push_back(nums[i]);
-                st.insert(i);
-                solve(nums, temp, st);
+        for (auto [num , freq] : mp) {
+            
+            if(freq > 0){
+                temp.push_back(num);
+                mp[num]--;
+                solve(temp , mp);
                 temp.pop_back();
-                st.erase(i);
+                mp[num]++;
             }
         }
     }
@@ -28,26 +28,31 @@ public:
 
         n = nums.size();
         vector<int> temp;
-        unordered_set<int> st;
-        solve(nums, temp, st);
+        unordered_map<int,int> mp;
 
-        sort(begin(res), end(res));
-
-        vector<vector<int>> ans;
-
-        for (int i = 0; i < res.size(); i++) {
-            vector<int> check =
-                res[i]; // Take the current permutation as 'check'
-
-            // Skip over duplicates in 'res'
-            while (i + 1 < res.size() && check == res[i + 1]) {
-                i++;
-            }
-
-            ans.push_back(check); // Add the unique permutation to 'ans'
+        for(int &num : nums){
+            mp[num]++;
         }
 
-        // Return the result containing only unique permutations
-        return ans;
+        solve( temp, mp);
+
+        // sort(begin(res), end(res));
+
+        // vector<vector<int>> ans;
+
+        // for (int i = 0; i < res.size(); i++) {
+        //     vector<int> check =
+        //         res[i]; // Take the current permutation as 'check'
+
+        //     // Skip over duplicates in 'res'
+        //     while (i + 1 < res.size() && check == res[i + 1]) {
+        //         i++;
+        //     }
+
+        //     ans.push_back(check); // Add the unique permutation to 'ans'
+        // }
+
+        // // Return the result containing only unique permutations
+        return res;
     }
 };
