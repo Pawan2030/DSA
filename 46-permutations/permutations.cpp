a3611b4caa10e8066ef1049a6e7d26dcb7fbbcf3
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-08-28 01:30:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (13.93%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,36 +1,59 @@
 class Solution {
 public:
-    
     vector<vector<int>> res;
     int n;
 
-    void solve(vector<int>& nums , vector<int> temp , unordered_set<int> st){
-        
-        //base case
-        if(temp.size() == n){
+ void solve(vector<int>& temp,  unordered_map<int,int>& mp) {
+
+        // base case
+        if (temp.size() == n) {
             res.push_back(temp);
             return;
         }
 
-        //logic
-        for(int i=0; i<n; i++){
-
-            if(st.find(nums[i]) == st.end()){
-                temp.push_back(nums[i]);
-                st.insert(nums[i]);
-                solve(nums , temp , st);
+        // logic
+        for (auto [num , freq] : mp) {
+            
+            if(freq > 0){
+                temp.push_back(num);
+                mp[num]--;
+                solve(temp , mp);
                 temp.pop_back();
-                st.erase(nums[i]);
+                mp[num]++;
             }
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
-     
-     n = nums.size();
-     vector<int> temp;
-     unordered_set<int> st;
-     solve(nums , temp , st);
-     return res;  
+    
+        n = nums.size();
+        vector<int> temp;
+        unordered_map<int,int> mp;
+
+        for(int &num : nums){
+            mp[num]++;
+        }
+
+        solve( temp, mp);
+
+        // sort(begin(res), end(res));
+
+        // vector<vector<int>> ans;
+
+        // for (int i = 0; i < res.size(); i++) {
+        //     vector<int> check =
+        //         res[i]; // Take the current permutation as 'check'
+
+        //     // Skip over duplicates in 'res'
+        //     while (i + 1 < res.size() && check == res[i + 1]) {
+        //         i++;
+        //     }
+
+        //     ans.push_back(check); // Add the unique permutation to 'ans'
+        // }
+
+        // // Return the result containing only unique permutations
+        return res;
+ 
     }
 };
