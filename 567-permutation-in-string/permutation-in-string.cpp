ee49360465756ae79e2b751fb7ac414ddb03cc03
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-10-08 14:41:25 +0530

    Time: 8 ms (56.15%) | Memory: 8.8 MB (75.80%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,21 +1,50 @@
 class Solution {
 public:
 
+    bool compare(vector<int>& freq1 , vector<int>& freq2){
+
+        for(int i=0; i<26; i++){
+
+            if(freq1[i] != freq2[i]) return false;
+        }
+
+        return true;
+    }
+
     bool checkInclusion(string s1, string s2) {
         
-        if(s1.length() > s2.length()) return false;
-        
-        sort(begin(s1) , end(s1));
-        int n = s1.length();
+        int n1 = s1.length();
+        int n2 = s2.length();
 
-        for(int i = 0; i<=s2.length()-n; i++){
+        if(n1 > n2) return false;
 
-            //make sub-string size = n
+        vector<int> freq1(26,0);
+    
+        for(char ch : s1){
+            freq1[ch-'a']++;
+        }
 
-            string st = s2.substr(i , n);
-            sort(begin(st) , end(st));
+        int i = 0;
+        int j = 0;
+        vector<int> freq2(26,0);
 
-            if(s1 == st) return true;
+        while(j < n2){
+            
+            freq2[s2[j] - 'a']++;
+
+            while(j-i+1 > n1){
+               freq2[s2[i] - 'a']--;
+               i++;
+            }
+
+            if(j-i+1 == n1){
+
+                bool res = compare(freq1 , freq2);
+
+                if(res) return true;
+            }
+
+            j++;
         }
 
         return false;
