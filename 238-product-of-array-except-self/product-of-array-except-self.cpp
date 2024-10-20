e81ef325aad74cf33c2e23fe6f9b9f879e94cc66
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-10-21 00:10:36 +0530

    Time: 0 ms (100.00%) | Memory: 38.4 MB (56.19%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -5,21 +5,17 @@ public:
         
         int n = nums.size();
 
-        vector<int> left(n , 1);
-        vector<int> right(n, 1);
-
+        vector<int> res(n , 1);
+       
         for(int i=1; i<n; i++){
-            left[i] = left[i-1]*nums[i-1];
+            res[i] = res[i-1]*nums[i-1];
         }
 
-        for(int i=n-2; i>=0; i--){
-            right[i] = right[i+1]*nums[i+1];
-        }
+        int rp = 1;
 
-        vector<int> res(n);
-
-        for(int i=0; i<n; i++){
-            res[i] = left[i]*right[i];
+        for(int i=n-1; i>=0; i--){
+            res[i] = res[i]*rp;
+            rp = rp*nums[i];
         }
 
         return res;
