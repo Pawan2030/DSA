4fa8b74de36f81a67a43547ccdbb1dc5c7163a54
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-11-01 16:34:29 +0530

    Time: 88 ms (11.79%) | Memory: 14.7 MB (95.51%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
-    int minimumMountainRemovals(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp1(n,0);
+    
+    int n;
 
-        for(int i=1;i<n;i++){
-            for(int prev=0;prev<i;prev++){
-                if(nums[prev]<nums[i] && dp1[i]<dp1[prev]+1){
-                    dp1[i]=(dp1[prev]+1);
+    void findmaxLIS(vector<int>& maxLIS , vector<int> &nums){
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(nums[j] < nums[i]){
+                    maxLIS[i] = max(maxLIS[i] , maxLIS[j] + 1);
                 }
             }
         }
-        vector<int>dp2(n,0);
-        for(int i=n-2;i>=0;i--){
-            for(int prev=n-1;prev>=i;prev--){
-                if(nums[prev]<nums[i] && dp2[i]<dp2[prev]+1){
-                    dp2[i]=(dp2[prev]+1);
+    }
+
+      void findmaxLDS(vector<int>& maxLDS , vector<int> &nums){
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=n-1; j > i; j--){
+                if(nums[j] < nums[i]){
+                    maxLDS[i] = max(maxLDS[i] , maxLDS[j] + 1);
                 }
             }
         }
-        int mx=0;
-        for(int i=0;i<n;i++){
-            if(dp1[i]>0 && dp2[i]>0){
-                mx=max(mx,dp1[i]+dp2[i]);
-            }
-            
-        }
-        return n-mx-1;
+    }
+
+    int minimumMountainRemovals(vector<int>& nums) {
+
+      n = nums.size();
+      vector<int> maxLIS(n , 1);
+      findmaxLIS(maxLIS , nums); 
+      vector<int> maxLDS(n , 1);
+      findmaxLDS(maxLDS , nums);   
+      
+      int res = n;
+
+      for(int i=0; i<n; i++){
+         if(maxLIS[i] > 1 && maxLDS[i] > 1)
+            res = min(res , n - maxLIS[i] - maxLDS[i] + 1);
+      }
+      return res;
     }
 };
