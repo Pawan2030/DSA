98395bcfc00650ab8d9effdb87d0a37ac76ed553
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-11-05 21:46:03 +0530

    Time: 3 ms (42.55%) | Memory: 12.1 MB (87.26%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
-    int minChanges(string& s) {
-        const int n=s.size();
-        int cnt=0;
-        for(int i=0; i<n; i+=2)
-            cnt+=(s[i]!=s[i+1]);
+    int minChanges(string s) {
+        
+        int n = s.length();
+        int cnt = 0;
+
+        for(int i=0; i<n-1; i+=2){
+            if(s[i] != s[i+1]){
+                cnt += 1;
+            }
+        }
         return cnt;
     }
 };
