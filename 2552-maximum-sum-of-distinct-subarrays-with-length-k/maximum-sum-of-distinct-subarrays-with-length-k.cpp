addcdd6c1ad794bd8b3e3a1875b33216a967e0df
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-11-20 02:49:30 +0530

    Time: 127 ms (31.69%) | Memory: 99.7 MB (36.21%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,53 +1,45 @@
 class Solution {
 public:
-
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        
-        long long sum = 0;
+
+        unordered_set<int> st;
+        int n = nums.size();
         long long res = 0;
-        int n  = nums.size();
-        int unique = 0;
+        long long sum = 0;
 
-        unordered_map<int,int> mp;
-        int i=0;
-        int j=0;
+        int i=0, j=0;
 
-        while(j < k){
-           int ele = nums[j];
-           mp[ele]++;
+        // while(j < k){
 
-           if(mp[ele] == 1){
-             unique++;
-           }
-           sum += ele;
-           j++;
-        }
-        
-        if(unique == k)
-         res = max(res , sum);
+        //     sum += nums[j];
+        //     st.insert(nums[j]);
+        //     j++;
+        // } 
+
+        // if(st.size() == k){
+        //     res = max(res , sum);
+        // }
 
         while(j < n){
 
-            mp[nums[j]]++;
-
-            if(mp[nums[j]] == 1){
-                unique++;
+            while(st.count(nums[j])){
+                sum -= nums[i];
+                st.erase(nums[i]);
+                i++;
             }
 
             sum += nums[j];
-            sum -= nums[i];
+            st.insert(nums[j]);
+        
+            if(k == (j-i+1)){
 
-            if(mp[nums[i]] == 1){
-               unique -= 1;
+                res = max(res , sum);
+
+                sum -= nums[i];
+                st.erase(nums[i]);
+                i++;
             }
             
-            mp[nums[i]]--;
-            i++;
-
-            if(unique == k){
-                res = max(res , sum);
-            }
-
             j++;
         }
 
