5e64d89abe986a84f8ed29f53b97a2cb440d24df
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-12-14 23:03:18 +0530

    Time: 242 ms (17.67%) | Memory: 156.1 MB (7.00%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.cpp
+++ 2868-continuous-subarrays/continuous-subarrays.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
+        
         long long res = 0;
-        int i = 0, j = 0;
+        int i = 0 , j = 0;
         int n = nums.size();
 
-        // Use a multiset to handle duplicates
-        multiset<int> st;
+        //set for maintaining max and min
+        multiset<int> mp;
 
-        while (j < n) {
-            st.insert(nums[j]);
+        while(j < n){
 
-            // Adjust `i` to maintain the condition `max - min <= 2`
-            while (*st.rbegin() - *st.begin() > 2) {
-                st.erase(st.find(nums[i])); // Erase one occurrence of nums[i]
+            mp.insert(nums[j]);
+
+            while(*mp.rbegin() - *mp.begin() > 2){
+                mp.erase(mp.find(nums[i]));
                 i++;
             }
 
-            // All subarrays between `i` and `j` are valid
             res += (j - i + 1);
-
             j++;
         }
-
         return res;
     }
 };
