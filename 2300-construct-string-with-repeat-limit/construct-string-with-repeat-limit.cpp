9215ecae335032c40a5c7e48c168a42e9092d25a
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-12-18 01:17:36 +0530

    Time: 24 ms (80.29%) | Memory: 28.1 MB (33.10%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -1,31 +1,49 @@
 class Solution {
 public:
-    string repeatLimitedString(string s, int k) {
-        vector<int> freq(26, 0);
-        for (char c : s) freq[c - 'a']++;
+    string repeatLimitedString(string s, int repeatLimit) {
         
-        priority_queue<pair<char, int>> pq;
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] > 0) pq.push({'a' + i, freq[i]});
+        vector<int> cnt(26 , 0);
+
+        for(char &ch : s){
+            cnt[ch-'a']++;
+        }
+
+        priority_queue<char> pq;
+
+        for(int i=0; i<26; i++){
+            char ch = 'a' + i;
+            if(cnt[i] > 0)
+             pq.push(ch);
         }
         
-        string result;
-        while (!pq.empty()) {
-            auto [ch, count] = pq.top(); pq.pop();
-            int used = min(k, count);
-            result.append(used, ch);
-            count -= used;
+        string res;
 
-            if (count > 0) {
-                if (pq.empty()) break;
-                auto [nextCh, nextCount] = pq.top(); pq.pop();
-                result += nextCh;
-                nextCount--;
+        while(!pq.empty()){
+           
+           char ch = pq.top();
+           pq.pop();
+           int freq = cnt[ch-'a'];
 
-                if (nextCount > 0) pq.push({nextCh, nextCount});
-                pq.push({ch, count});
-            }
+           int k = min(freq , repeatLimit);
+           cnt[ch-'a'] -= k;
+
+           res.append(k , ch);
+
+           if(cnt[ch-'a'] > 0 && !pq.empty()){
+               
+               char chh = pq.top();
+               pq.pop();
+
+               res.append(1 , chh);
+               cnt[chh-'a']--;
+
+               if(cnt[chh-'a'] > 0){
+                 pq.push(chh);
+               }
+
+               pq.push(ch);
+           }
         }
-        return result;
+        return res;
     }
 };
