d90719a323c2f960cd2ad0531933c06756008f38
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2024-12-19 22:14:32 +0530

    Time: 4 ms (100.00%) | Memory: 9.4 MB (7.32%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -2,32 +2,57 @@ class Solution {
 public:
 
     int maxChunksToSorted(vector<int>& arr) {
+ 
+        int sum = 0;
+        int n = arr.size();
+        vector<int> temp(n , 0);
 
-       int n = arr.size();
-       vector<int> mini(n);
-       vector<int> maxi(n); 
-       int val = -1;
+        for(int i=0; i<n; i++){
+            sum += i;
+            temp[i] = sum;
+        }
+        
+        int cum = 0;
+        int chunk = 0;
 
-       for(int i=0; i<n; i++){
-          val = max(arr[i] , val);
-          maxi[i] = val;
-       }
-       
-       val = n;
-
-       for(int i=n-1; i>=0; i--){
-          val = min(val , arr[i]);
-          mini[i] = val; 
-       }
-
-       int chunk = 1;
-
-       for(int i=1; i<n; i++){
-          if(maxi[i-1] < mini[i]){
-            chunk++;
-          }
-       }
-
-       return chunk;
+        for(int i=0; i<n; i++){
+            cum = cum + arr[i];
+            cout<<"ADD->>"<<cum<<endl;
+            if(cum == temp[i]){
+                cout<<"cum->"<<cum<<endl;
+                cout<<"temp->"<<temp[i]<<endl;
+                chunk++;
+            }
+        }
+        return chunk;
     }
 };
+
+//        int n = arr.size();
+//        vector<int> mini(n);
+//        vector<int> maxi(n); 
+//        int val = -1;
+
+//        for(int i=0; i<n; i++){
+//           val = max(arr[i] , val);
+//           maxi[i] = val;
+//        }
+       
+//        val = n;
+
+//        for(int i=n-1; i>=0; i--){
+//           val = min(val , arr[i]);
+//           mini[i] = val; 
+//        }
+
+//        int chunk = 1;
+
+//        for(int i=1; i<n; i++){
+//           if(maxi[i-1] < mini[i]){
+//             chunk++;
+//           }
+//        }
+
+//        return chunk;
+//     }
+// };
