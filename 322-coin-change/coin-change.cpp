432da71711e1cd27a658ff9f581133031c274837
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-01-16 19:50:33 +0530

    Time: 43 ms (39.56%) | Memory: 53 MB (14.10%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,38 +1,47 @@
 class Solution {
 public:
-    
-    int n;
-    int dp[13][10001];
-
-    int solve(int i, vector<int>& coins, int amount){
-
-        if(amount == 0){
-            return 0;
-        }
-
-        if(i >= n || amount < 0){
-            return 1e9;
-        }
-
-        if(dp[i][amount] != -1){
-            return dp[i][amount];
-        }
-        
-        //int take = 1e5;
-        //if(amount >= coins[i]){
-           int take = 1+solve(i, coins, amount-coins[i]);
-        
-           
-        int skip = solve(i+1, coins, amount);
-
-        return dp[i][amount] = min(take , skip);
-    }
 
     int coinChange(vector<int>& coins, int amount) {
 
-       n = coins.size();
-       memset(dp , -1, sizeof(dp));
-       int ans = solve(0 , coins , amount); 
-       return ans == 1e9 ? -1 : ans;
+       int n = coins.size();
+
+       vector<vector<int>> dp(n+1, vector<int>(amount+1)) ;
+
+       //fill 0th col, we need to make 0 rs so it alredy 0 so we need 0 coins
+        for(int i=0; i<=n; i++){
+          dp[i][0] = 0;
+       }
+
+       //let fill 0th row where we have 0 elements so we cant make that amount it means we need infinite coins to make
+       
+       for(int i=0; i<=amount; i++){
+          dp[0][i] = INT_MAX-1;
+       }
+
+       //lest fill the second row -> special case
+       for(int val=1; val<=amount; val++){
+           
+           if(coins[0] != 0 && val%coins[0] == 0){
+              dp[1][val] = val/coins[0];
+           }
+           else{
+              dp[1][val] = INT_MAX-1;
+           }
+        }
+
+        //fill rest dp matrix row - 2 and col - 1
+        for(int i=2; i<=n; i++){
+            for(int j=1; j<=amount; j++){
+
+                if(j >= coins[i-1]){
+                    dp[i][j] = min(1+dp[i][j-coins[i-1]] , dp[i-1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][amount] == INT_MAX-1 ? -1 : dp[n][amount];
     }
 };
