92059bb546e7354e1360f66b9cb980e80eb6da11
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-01-18 00:29:55 +0530

    Time: 44 ms (5.14%) | Memory: 263.8 MB (90.19%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    bool doesValidArrayExist(vector<int>& arr) {
+
+        int n = arr.size();
+        bool res = true;
+        if(n==1 && arr[0] == 1) return false;
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+           if(arr[i%n] == 1){
+              if(arr[(i+1)%n] != 1){
+                 if((i==0 && arr[n-1] != 1) || (i > 0 && arr[i-1] != 1)){
+                    res = false;
+                 }
+              }
+           }
+        }
+        
+        for(int i : arr){
+            if(i==1) cnt++;
+        }
+        
+        if(cnt%2 != 0) return false;
+
+        return true;
+    }
+};
