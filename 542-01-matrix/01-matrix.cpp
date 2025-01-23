4f5cfddc4c8eaffccffc9ea58754971ded4dfbec
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-01-23 19:35:41 +0530

    Time: 179 ms (6.37%) | Memory: 74.8 MB (5.21%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
-    
-    int m , n;
 
-    vector<vector<int>> arr{{1,0}, {-1,0}, {0 , 1} , {0 , -1}};
+     vector<vector<int>> directions{
+       {0,1},{0,-1},{1,0},{-1,0}
+     };
 
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-      
-      m = mat.size();
-      n = mat[0].size();
+        
+        //it kind of multi-source BFS
+        int m = mat.size();
+        int n = mat[0].size();
 
-      vector<vector<int>> ans(m , vector<int>(n , -1));
-      queue<pair<int, int>> q;
+         vector<vector<int>> res(m , vector<int>(n,-1));
+         queue<pair<int,int>> q;
 
-      for(int i = 0; i<m; i++){
-        for(int j = 0; j<n; j++){
-
-            if(mat[i][j] == 0){
-                ans[i][j] = 0;
-                q.push({i , j});
-            }
-        }
-      }
-  
-      while(!q.empty()){
-         
-         auto it = q.front();
-         q.pop();
-
-         int s = it.first;
-         int e = it.second;
-         
-         for(auto &ch : arr){
-
-            int x = s + ch[0];
-            int y = e + ch[1];
-
-            if(x >= 0 && y >= 0 && x < m && y < n && ans[x][y] == -1){
-                ans[x][y] = 1 + ans[s][e];
-                q.push({x , y});
+         for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j] == 0){
+                    res[i][j] = 0;
+                    q.push({i,j});
+                }
             }
          }
-      }
-        return ans;
+
+         while(!q.empty()){
+
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+
+            for(auto dir : directions){
+
+                int x = dir[0] + i;
+                int y = dir[1] + j;
+
+                if(x >= 0 && x < m && y >= 0 && y < n && res[x][y] == -1){
+                    res[x][y] = res[i][j]+1;
+                    q.push({x,y});
+                }
+            }
+
+         }
+         return res;
     }
 };
