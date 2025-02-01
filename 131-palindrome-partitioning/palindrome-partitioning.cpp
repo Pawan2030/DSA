2fc9c64dc5795e3057b199539c3d677d6e96a40f
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-02-01 19:59:48 +0530

    Time: 51 ms (54.59%) | Memory: 80 MB (26.41%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,48 +1,48 @@
 class Solution {
 public:
+    
+    vector<vector<string>> res;
+    int n;
 
-    bool isPalindrome(string &st , int s, int e){
+    bool isPalindrome(string &check){
 
-         while(s < e){
+        int i = 0;
+        int j = check.length()-1;
 
-            if(st[s] != st[e]){
+        while(i < j){
+            if(check[i] != check[j]){
                 return false;
             }
-            s++;
-            e--;
-         }
-
-         return true;
+            i++;
+            j--;
+        }
+        return true;
     }
-   
-    void solve(int idx , string &s , int n , vector<vector<string>> &result , vector<string> &temp){
-         
-          if(idx >= n){
-            result.push_back(temp);
-            return;
-          }
 
-          for(int i=idx ; i<n; i++){
-              
-              if(isPalindrome(s , idx , i)){
-                 temp.push_back(s.substr(idx , i - idx + 1));
-                 solve(i+1 , s , n , result , temp);
-                 temp.pop_back();
-              }
-          }
+    void solve(int i, vector<string> &temp, string& s){
 
+       if( i >= n){
+          res.push_back(temp);
           return;
+       }
+
+       for(int l=1; l+i<=n; l++){
+
+        string check = s.substr(i , l);
+        if(isPalindrome(check)){
+            temp.push_back(check);
+            solve(i+l,temp,s);
+            temp.pop_back();
+        }
+
+       }
+
     }
-    
+
     vector<vector<string>> partition(string s) {
-
-       int n = s.size();  
-       vector<vector<string>> result;
-
-       vector<string> temp;
-
-       solve(0 , s , n , result , temp);
-
-       return result;
+        vector<string> temp;
+        n = s.size();
+        solve(0, temp , s);
+        return res;   
     }
 };
