228a9336ac2ef4b5dbb33a0e8edbf381e716f047
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-02-05 13:18:55 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (28.34%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,59 +1,37 @@
 class Solution {
-public: 
+public:
     
     int n;
-    vector<vector<int>> ans;
     vector<vector<int>> res;
-    
 
-    void solve(int i , vector<int>& can , int tar , vector<int>& temp , int& sum){
+    void solve(int idx , vector<int>& can, int tar, vector<int>& temp){
         
-         if(sum == tar){
-            ans.push_back(temp);
+        if(tar < 0){
             return;
         }
 
-        if(i >= n || sum > tar){
+        if(tar == 0){
+            res.push_back(temp);
             return;
         }
 
-        if(sum <= tar){
+        for(int i=idx; i<n; i++){
+            
+            if(i > idx && can[i] == can[i-1]) continue;
+
             temp.push_back(can[i]);
-            sum += can[i];
-            solve(i+1 , can , tar , temp , sum);
-            sum -= can[i];
+            solve(i+1,can,tar-can[i],temp);
             temp.pop_back();
         }
 
-        while(i+1 < n && can[i] == can[i+1]) i++;
-
-        solve(i+1 , can , tar , temp , sum);
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         
         n = candidates.size();
-        vector<int> temp;
-        int sum = 0;
-
         sort(begin(candidates) , end(candidates));
-
-        solve(0 , candidates , target, temp , sum);
-
-        //sort(begin(ans) , end(ans));
-        
-       // int i =0;
-
-        // while(i < ans.size()){
-
-        //     auto it = ans[i];
-
-        //     while(i < ans.size() && it == ans[i]){
-        //         i++;
-        //     }
-
-        //     res.push_back(it);
-        // }
-        return ans;
+        vector<int> temp;
+        solve(0,candidates,target,temp);
+        return res;
     }
 };
