b3db03067da6d6d1a87629efe34f0dfc73de494d
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-02-08 16:16:01 +0530

    Time: 3 ms (29.82%) | Memory: 11.4 MB (26.92%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,59 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> res;
+    
     int n;
+    vector<vector<int>> res;
 
- void solve(vector<int>& temp,  unordered_map<int,int>& mp) {
+    void solve(vector<int>& temp , vector<int>& nums, unordered_set<int>& st){
 
-        // base case
-        if (temp.size() == n) {
+        if(temp.size() == n){
             res.push_back(temp);
             return;
         }
-
-        // logic
-        for (auto [num , freq] : mp) {
-            
-            if(freq > 0){
-                temp.push_back(num);
-                mp[num]--;
-                solve(temp , mp);
+        
+        for(int i=0; i<n; i++){
+            if(st.find(nums[i]) == st.end()){
+                temp.push_back(nums[i]);
+                st.insert(nums[i]);
+                solve(temp , nums, st);
                 temp.pop_back();
-                mp[num]++;
+                st.erase(nums[i]);
             }
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
-    
-        n = nums.size();
-        vector<int> temp;
-        unordered_map<int,int> mp;
-
-        for(int &num : nums){
-            mp[num]++;
-        }
-
-        solve( temp, mp);
-
-        // sort(begin(res), end(res));
-
-        // vector<vector<int>> ans;
-
-        // for (int i = 0; i < res.size(); i++) {
-        //     vector<int> check =
-        //         res[i]; // Take the current permutation as 'check'
-
-        //     // Skip over duplicates in 'res'
-        //     while (i + 1 < res.size() && check == res[i + 1]) {
-        //         i++;
-        //     }
-
-        //     ans.push_back(check); // Add the unique permutation to 'ans'
-        // }
-
-        // // Return the result containing only unique permutations
-        return res;
- 
+        
+       n = nums.size();
+       vector<int> temp;
+       unordered_set<int> st;
+       solve(temp , nums , st);
+       return res;
     }
 };
