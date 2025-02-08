1f744edd90cffe095f7dae23ec6d6b66eb296d7b
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-02-08 19:32:36 +0530

    Time: 2017 ms (5.01%) | Memory: 12.9 MB (43.95%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,58 +1,41 @@
 class Solution {
 public:
+     
     vector<vector<int>> res;
+    set<vector<int>> st;
     int n;
 
-    void solve(vector<int>& temp,  unordered_map<int,int>& mp) {
+    void solve(vector<int>& nums, vector<int>& temp, unordered_map<int,int>& mp){
 
-        // base case
-        if (temp.size() == n) {
+        if(temp.size() == n && st.find(temp) == st.end()){
             res.push_back(temp);
+            st.insert(temp);
             return;
         }
 
-        // logic
-        for (auto [num , freq] : mp) {
-            
-            if(freq > 0){
-                temp.push_back(num);
-                mp[num]--;
-                solve(temp , mp);
+        for(int i=0; i<n; i++){
+            if(mp[nums[i]] > 0){
+                mp[nums[i]]--;
+                temp.push_back(nums[i]);
+                solve(nums,temp,mp);
                 temp.pop_back();
-                mp[num]++;
+                mp[nums[i]]++;
             }
         }
+
     }
 
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-
+        
         n = nums.size();
         vector<int> temp;
         unordered_map<int,int> mp;
 
-        for(int &num : nums){
+        for(int num : nums){
             mp[num]++;
         }
 
-        solve( temp, mp);
-
-        // sort(begin(res), end(res));
-
-        // vector<vector<int>> ans;
-
-        // for (int i = 0; i < res.size(); i++) {
-        //     vector<int> check =
-        //         res[i]; // Take the current permutation as 'check'
-
-        //     // Skip over duplicates in 'res'
-        //     while (i + 1 < res.size() && check == res[i + 1]) {
-        //         i++;
-        //     }
-
-        //     ans.push_back(check); // Add the unique permutation to 'ans'
-        // }
-
-        // // Return the result containing only unique permutations
+        solve(nums,temp,mp);
         return res;
     }
 };
