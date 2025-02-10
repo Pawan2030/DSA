c1f874364849717bbdbdab76d3d26588c126d664
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-02-10 20:48:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (27.61%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    bool nonDigit(char &ch){
+        return (ch == '0' || ch == '1' || ch == '2' || ch == '3' || ch == '4' || ch == '5' || ch == '6' || ch == '7' || ch == '8' || ch == '9' );
+    }
+
+    string clearDigits(string s) {
+
+        int n = s.length();
+        stack<char> st;
+
+        for(char ch : s){
+
+            if(!st.empty() && nonDigit(ch)){
+                 st.pop();
+            }
+
+            if(!nonDigit(ch)){
+                st.push(ch);
+            }
+        }
+
+        string res = "";
+        
+        while(!st.empty()){
+            res += st.top();
+            st.pop();
+        }
+        
+        reverse(begin(res) , end(res));
+        return res;
+    }
+};
