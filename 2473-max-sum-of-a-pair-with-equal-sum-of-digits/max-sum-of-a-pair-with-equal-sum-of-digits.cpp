9504f0cf347bd3a6423942198e53553acc4d15fb
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-02-13 01:21:12 +0530

    Time: 18 ms (79.79%) | Memory: 67.2 MB (43.46%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -16,7 +16,7 @@ public:
 
     int maximumSum(vector<int>& nums) {
         
-        unordered_map<int,vector<int>> mp;
+        unordered_map<int,int> mp;
         int res = -1;
         int n = nums.size();
 
@@ -32,13 +32,12 @@ public:
             int sum = pre[i];
 
             if(mp.find(sum) != mp.end()){
-
-                for(int &val : mp[sum]){
-                    res = max(res , nums[i]+val);
-                }
+                int val = mp[sum];
+                res = max(res , nums[i]+val);
+                mp[sum] = max(val , nums[i]);;
+            }else{
+                mp[sum] = nums[i];
             }
-            
-            mp[sum].push_back(nums[i]);
         }
 
         return res;
