bd668f5f55a4f961e9933e94dfb9e9f4983a8c40
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-02-22 18:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (91.16%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,41 +1,51 @@
+
 class Solution {
 public:
-    
-    bool checkCycleCount(int u, int p, int val, vector<int>& cnt, vector<bool> &visited, vector<vector<int>>& graph) {
+
+bool checkCycleCount(int u, int p , int val , vector<int>& cnt ,  vector<bool> &visited , vector<vector<int>>& graph){
+
         visited[u] = true;
-        cout<<"u->"<<u<<endl;
-         cout<<"val->"<<val<<endl;
-        cnt[u] = val;  // Store level of node
+        cnt[u] = val;
 
-        for (int v : graph[u]) {
-            if (v == p) continue; // Skip the parent node
+        for(int v : graph[u]){
 
-            if (visited[v]) {
-                // If we visit an already visited node, check for odd cycle
-                cout<<"Insideval->"<<val<<endl;
-                 cout<<"Char->"<<v<<endl;
-                if (((val+1) - cnt[v]) % 2 != 0) { 
-                    return false; // Odd-length cycle found, not bipartite
-                }
-            } else {
-                if (!checkCycleCount(v, u, val + 1, cnt, visited, graph)) {
-                    return false;
-                }
+            if(v == p) continue;
+
+            if(visited[v]){
+
+               int p1 =  cnt[v];
+               int p2 =  val;
+
+               int data = p2-p1+1;
+
+               if(data%2 != 0){
+                   return false;
+               }
+
+            }
+            else{
+               bool get = checkCycleCount(v , u , val+1, cnt, visited, graph);
+
+               if(!get) return false;
             }
         }
-
+        
         return true;
     }
 
     bool isBipartite(vector<vector<int>>& graph) {
+        
         int n = graph.size();
-        vector<int> cnt(n, -1);
-        vector<bool> visited(n, false);
+        vector<int> cnt(n,-1);
+        vector<bool> visited(n , false);
+        int cycle = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                bool ans = checkCycleCount(i, -1, 1, cnt, visited, graph);
-                if (!ans) return false;
+
+        for(int i=0; i<n; i++){
+
+            if(!visited[i]){
+                bool ans = checkCycleCount(i , -1 , 1 , cnt , visited , graph);
+                if(!ans) return false;
             }
         }
         
