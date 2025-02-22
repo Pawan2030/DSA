27c42a984f71f440e38ebb60426d6e63d8a62ca5
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-02-22 13:56:26 +0530

    Time: 26 ms (64.66%) | Memory: 16.4 MB (66.75%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -9,7 +9,7 @@ public:
 
     void coverIsaland(int i, int j, vector<vector<char>>& grid){
         
-        //if((s < 0 || s >= m || e < 0 || e >= n || visited[s][e])) return;
+        if(i < 0 || i >= m || j < 0 || j>=n || grid[i][j] == '0') return;
 
         grid[i][j] = '0';
 
@@ -18,9 +18,9 @@ public:
             int x = i + dir[0];
             int y = j + dir[1];
             
-            if(x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == '1'){
+            //if(x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == '1'){
                 coverIsaland(x , y , grid);
-            }
+           // }
         }
 
     }
