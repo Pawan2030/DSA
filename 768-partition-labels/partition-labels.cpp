64214bf5f779c724ee46f1d3f5fdd3b94b398a74
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-03-31 01:24:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (97.09%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int> partitionSizes;
-        int lastOccurrence[26] = {0}, firstOccurrence[26] = {0};
-        int partitionStart = 0, partitionEnd = 0;
+        
+        int n = s.length();
+        vector<int> nums(26 , 0);
 
-        // Store the last occurrence index of each character
-        for (int i = 0; i < s.length(); i++) {
-            lastOccurrence[s[i] - 'a'] = i;
+        for(int i=0; i<n; i++){
+            nums[s[i]-'a'] = i;
         }
 
-        for (int i = 0; i < s.length(); i++) {
-            // Store the first occurrence index of each character (if not set)
-            if (!firstOccurrence[s[i] - 'a']) {
-                firstOccurrence[s[i] - 'a'] = i;
+        int i =0;
+        int j =0;
+        vector<int> res;
+
+        while(j<n){
+
+            i = j;
+            int lastEle = nums[s[j]-'a'];
+
+            while(j < lastEle){
+                j++;
+                int temp = nums[s[j]-'a'];
+                if(temp > lastEle){
+                    lastEle = temp;
+                }
             }
 
-            // If we find a new partition start
-            if (partitionEnd < firstOccurrence[s[i] - 'a']) {
-                partitionSizes.push_back(partitionEnd - partitionStart + 1);
-                partitionStart = i;
-                partitionEnd = i;
-            }
-
-            // Update partition end boundary
-            partitionEnd = max(partitionEnd, lastOccurrence[s[i] - 'a']);
+            res.push_back(j-i+1);
+            j++;
+            
         }
-
-        // Add the last partition if it exists
-        if (partitionEnd - partitionStart + 1 > 0) {
-            partitionSizes.push_back(partitionEnd - partitionStart + 1);
-        }
-
-        return partitionSizes;
+        return res;
     }
 };
