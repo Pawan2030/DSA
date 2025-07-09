6779633330b539c20b5d70a93ad2a64df26c06ae
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-07-09 12:59:00 +0530

    Time: 3 ms (35.84%) | Memory: 112.4 MB (71.90%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
-/*
 
-amount of gas at ith station = gas[i]
-cost i - i+1 = cost[i]
-
-
-*/
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-
+        
         int totalGas = 0;
         int currGas  = 0;
         int startIdx = 0;
@@ -16,16 +10,18 @@ cost i - i+1 = cost[i]
 
         for(int i=0; i<n; i++){
 
-            totalGas = totalGas + gas[i] - cost[i];
-            currGas  = currGas  + gas[i] - cost[i];
+            totalGas = totalGas + gas[i] - cost[i]; // i - i+1 the cost to traval b/w these station is cost[i] so we need to subtract this amout of gas
+            currGas = currGas + gas[i] - cost[i];
 
             if(currGas < 0){
-                startIdx = i+1;
                 currGas  = 0;
+                startIdx = i+1;
             }
+
         }
 
-        if(totalGas < 0) return -1;
+        if(totalGas < 0) return -1; // cost is hight to travel b/w stations so cant possibel to reach again on circuit same station where we started.
         return startIdx;
+
     }
 };
