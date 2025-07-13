747e1bd3782f018278373a01bf2b4788a639b538
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-07-13 16:53:19 +0530

    Time: 69 ms (20.43%) | Memory: 76 MB (17.14%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -1,28 +1,54 @@
 class Solution {
 public:
-    int maxEvents(vector<vector<int>>& events) {
-        int n = events.size();
-        int maxDay = 0;
-        for (int i = 0; i < events.size(); i++) {
-            maxDay = max(maxDay, events[i][1]);
+    
+    typedef pair<int,int> P;
+
+    struct custom{
+        bool operator() (P p1, P p2){
+            if(p1.second < p2.second){
+                return false;
+            }
+            return true;
         }
-        priority_queue<int, vector<int>, greater<>> pq;
-        sort(events.begin(), events.end());
-        int ans = 0;
-        for (int i = 0, j = 0; i <= maxDay; i++) {
-            while (j < n && events[j][0] <= i) {
-                pq.emplace(events[j][1]);
-                j++;
-            }
-            while (!pq.empty() && pq.top() < i) {
-                pq.pop();
-            }
-            if (!pq.empty()) {
-                pq.pop();
-                ans++;
-            }
+    };
+
+    int maxEvents(vector<vector<int>>& events) {
+        
+        int n = events.size();
+        int maxevent = 0;
+
+        priority_queue<P , vector<P> , custom> pq;
+        sort(begin(events) , end(events));
+
+        int minDay = INT_MAX;
+        int maxDay = INT_MIN;
+
+        for(int i=0; i<n; i++){
+            minDay = min(minDay , events[i][0]);
+            maxDay = max(maxDay , events[i][1]);
         }
 
-        return ans;
+        int idx = 0;
+
+        while(minDay <= maxDay){
+            
+            // on MinDay All event we are putting in min-Queue
+            while(idx < n && events[idx][0] <= minDay){
+                pq.push({events[idx][0] , events[idx][1]});
+                idx++;
+            }
+
+            while(!pq.empty() && pq.top().second < minDay){
+                pq.pop();
+            }
+            
+            if(!pq.empty()){
+               maxevent += 1;
+                pq.pop();
+            }
+            minDay += 1;
+        }
+
+        return maxevent;
     }
 };
