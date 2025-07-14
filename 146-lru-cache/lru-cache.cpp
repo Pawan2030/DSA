159afad739a0976802b9374519f3b01252f4c6a2
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-07-14 23:25:08 +0530

    Time: 101 ms (36.42%) | Memory: 182.2 MB (37.83%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -3,45 +3,49 @@ public:
     
     int n;
     list<int> dll;
-    unordered_map<int,pair<list<int> :: iterator , int >> mp;  // key -> ADD , Val
+    unordered_map<int , pair<list<int> :: iterator , int>> mp;
 
     LRUCache(int capacity) {
        n = capacity;
     }
     
     int get(int key) {
-        
-        //present so return val of that input key
+
         if(mp.find(key) != mp.end()){
 
-            auto it = mp[key].first;
-            int val = mp[key].second;
-            dll.erase(it);
+            auto address = mp[key].first;
+            dll.erase(address);
             dll.push_front(key);
-            mp[key] = {dll.begin() , val};
+            mp[key].first = dll.begin();
             return mp[key].second;
         }
-
-        //not present
         return -1;
     }
     
     void put(int key, int value) {
 
-        //alredy presnt with this key - val so make it most freq used and update in map
         if(mp.find(key) != mp.end()){
+           
+           auto address = mp[key].first;
+           dll.erase(address);
+           dll.push_front(key);
+           mp[key] = {dll.begin() , value};
+        }
+        else if(n >= 0){
 
-            auto it = mp[key].first;
-            dll.erase(it);
-            
-        }
-        else if(mp.size() == n){ // remove least and add new
-            auto it = dll.back();
-            dll.pop_back();
-            mp.erase(it);
-        }
             dll.push_front(key);
-            mp[key] = {dll.begin() , value}; 
+            mp[key] = {dll.begin() , value};
+            //n--;
+        }
+
+        if(mp.size() > n){
+
+            int LRU = dll.back();
+            dll.pop_back();
+            mp.erase(LRU);
+           // n++;
+        }
+        
     }
 };
 
