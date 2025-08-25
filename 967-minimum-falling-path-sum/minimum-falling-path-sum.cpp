7a0c38fb2f6a30c3f24e882ffa815b66ad8eae77
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-08-25 16:49:31 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (82.63%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& matrix) {
+    int minFallingPathSum(vector<vector<int>>& mat) {
         
-        int m = matrix.size();
-        int n = matrix[0].size();
+        int m = mat.size();
+        int n = mat[0].size();
 
         vector<int> dp(n);
 
-        for(int j=0; j<n; j++){
-            dp[j] = matrix[0][j];
+        for(int i=0; i<n; i++){
+            dp[i] = mat[0][i];
         }
 
-        for(int i = 1; i<m; i++){
-            
+
+        for(int i=1; i<m; i++){
+
             vector<int> curr(n);
 
-            for(int j = 0; j<n; j++){
-                
+            for(int j=0; j<n; j++){
+
                 if(j == 0){
-                    curr[j] = matrix[i][j] + min(dp[j] , dp[j+1]);
+                    curr[j] = mat[i][j] + min(dp[j] , dp[j+1]);
                 }
                 else if(j == n-1){
-                    curr[j] = matrix[i][j] + min(dp[j] , dp[j-1]);
+                    curr[j] = mat[i][j] + min(dp[j] , dp[j-1]);
                 }
                 else{
-                    curr[j] = matrix[i][j] + min({dp[j-1] , dp[j] , dp[j+1]});
+                    curr[j] = mat[i][j] + min({dp[j] , dp[j-1] , dp[j+1]});
                 }
-
             }
+
             dp = curr;
+
         }
 
-        return *min_element(begin(dp) , end(dp));
+        return *min_element(dp.begin() , dp.end());
     }
 };
