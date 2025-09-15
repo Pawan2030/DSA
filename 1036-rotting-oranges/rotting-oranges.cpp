e74ee2d97b93ea6d49dc45ea3ae748e8803d8b2e
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-09-15 23:41:29 +0530

    Time: 4 ms (15.99%) | Memory: 18.1 MB (5.63%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
-     typedef pair<int,int> P;
-
-     vector<vector<int>> dimension = {
-        {0,1} , {0, -1} , {1 , 0} , {-1 , 0}
-     };
-
-     bool isPossible(int newX , int newY , int m , int n){
-        if(newX >= 0 && newX < m && newY >= 0 && newY < n) return true;
-        return false;
-     }
-
-
-     /*
-
-     1. decler mint = 0 time and also store all rotate orange coo-dinate in queue for future adcent process
-     2. take a size every time with incresing time+1 mint also check the out of box grid adjecnt
-     */
+    
+    vector<vector<int>> directions = {
+      {0,1}, {0, -1}, {1 , 0}, {-1,0}
+    };
 
     int orangesRotting(vector<vector<int>>& grid) {
         
         int m = grid.size();
         int n = grid[0].size();
-        queue<P> q;
+
+        queue<pair<int,int>> q;
+
+        //put all the rotten Oragen in the Queue
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
@@ -31,49 +21,42 @@ public:
                 }
             }
         }
-
+        
         int mint = 0;
 
         while(!q.empty()){
-
+            
             int size = q.size();
-            bool nonRotate = false;
+            bool Rotate = false;
+            for(int k=0; k<size; k++){
+                 auto pair = q.front();
+                 q.pop();
 
-            for(int i=0; i<size; i++){
-                
-                P pos = q.front();
-                q.pop();
+                 for(auto dir : directions){
+                      
+                      int newRow = pair.first  + dir[0];
+                      int newCol = pair.second + dir[1];
 
-                int x = pos.first;
-                int y = pos.second;
-
-                for(auto &it : dimension){
-
-                    int newX = x + it[0];
-                    int newY = y + it[1];
-                    
-                    if(isPossible(newX , newY , m , n) && grid[newX][newY] == 1){
-                        grid[newX][newY] = 2;
-                        q.push({newX , newY});
-                        nonRotate = true;
-                    }
-                }
+                      if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && grid[newRow][newCol] == 1){
+                           q.push({newRow , newCol});
+                           grid[newRow][newCol] = 2;
+                           Rotate = true;
+                      }
+                 }
             }
 
-            if(nonRotate)
+            if(Rotate == true)
              mint++;
         }
-
-        int fresh = 0;
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] == 1){
-                    return -1;
+                   return -1;
                 }
             }
         }
 
-        return mint;  
+        return mint;
     }
 };
