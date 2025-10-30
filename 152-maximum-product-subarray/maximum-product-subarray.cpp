802696e174efebbc235fa2ad6f998e4ec5676d66
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-10-30 20:04:01 +0530

    Time: 399 ms (5.60%) | Memory: 17.8 MB (59.69%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,43 +1,21 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        //frute force
         
         int n = nums.size();
-        int l = 1;
-        int r = 1;
-        int res = INT_MIN;
+        int maxi = INT_MIN;
 
         for(int i=0; i<n; i++){
 
-            l = l == 0 ? 1 : l;
-            r = r == 0 ? 1 : r;
-             
-            l *= nums[i];
-            r *= nums[n-i-1];
-            res = max({res , l , r , nums[i]});
-            
-        }
+            int temp = 1;
 
-        return res;
-    }
-};
-
-//broot force->
-
-//-> try through every subset and track the max product from every subset
-// time complexity->>>>> o(n*n)
-
-/*
-
- int res = INT_MIN;
- int n = nums.size();
-        for(int i=0; i<n; i++){
-            long long pro = 1;
             for(int j=i; j<n; j++){
-               pro *= nums[j];
-               res = fmax(res,pro);
+                 temp *= nums[j];
+                 maxi = max(maxi , temp);
             }
         }
-    return res;
 
-*/
+        return maxi;
+    }
+};
