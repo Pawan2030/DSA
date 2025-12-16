3b4e17b07d5f6b2d8fff7523bc4c6b5547a945a1
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-12-17 01:14:39 +0530

    Time: 3 ms (89.97%) | Memory: 24 MB (34.42%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
+
     vector<vector<int>> merge(vector<vector<int>>& in) {
         
+        sort(in.begin() , in.end());
         int n = in.size();
-        sort(begin(in) , end(in));
         vector<vector<int>> res;
 
-        int mini = in[0][0];
-        int maxi = in[0][1];
+        int sp = in[0][0];
+        int se = in[0][1];
 
         for(int i=1; i<n; i++){
 
-            int minEle = in[i][0];
-            int maxEle = in[i][1];
+            int es = in[i][0];
+            int ee = in[i][1];
 
-            if(maxi >= minEle){
-                mini =  min(mini , minEle);
-                maxi =  max(maxi , maxEle);
+            if(se >= es){
+                sp = min(sp , es);
+                se = max(se , ee);
             }
             else{
-                res.push_back({mini , maxi});
-                mini = minEle;
-                maxi = maxEle;
+                res.push_back({sp , se});
+                sp = es;
+                se = ee;
             }
         }
-
-        res.push_back({mini , maxi});
+        
+        res.push_back({sp , se});
         return res;
     }
 };
