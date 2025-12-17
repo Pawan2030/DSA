ffbbd99882cae10bb66f0e00ff88ca71f7e357ed
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-12-18 01:43:35 +0530

    Time: 31 ms (36.03%) | Memory: 13 MB (84.02%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int n1,n2;
+    
+    int n1 , n2;
     int t[1001][1001];
 
     int solve(int i , int j , string &text1 , string &text2){
 
         if(i >= n1 || j >= n2) return 0;
-        
+
         if(t[i][j] != -1) return t[i][j];
+
         if(text1[i] == text2[j]){
-            return t[i][j] = 1+solve(i+1 , j+1 , text1 , text2);
+            return t[i][j] =  1 + solve(i+1, j+1 , text1 , text2);
         }
-        else{
-            return t[i][j] =  max(solve(i+1 , j , text1 , text2) , solve(i , j+1 , text1 , text2));
-        }
+
+        return t[i][j] = max(solve(i+1,j,text1,text2) , solve(i,j+1,text1,text2));
     }
 
     int longestCommonSubsequence(string text1, string text2) {
+        
+        n1 = text1.size();
+        n2 = text2.size();
+        memset(t , -1 , sizeof(t));
 
-       n1 = text1.length();
-       n2 = text2.length();
-       memset(t , -1 , sizeof(t));
-       return solve(0 , 0 , text1 , text2); 
+        return solve(0 , 0 , text1 , text2);
     }
 };
