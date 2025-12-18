2e479c94551742db85ae15ed0cfa65e47b3df7b0
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-12-18 17:09:13 +0530

    Time: 504 ms (21.46%) | Memory: 41.6 MB (32.15%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,38 +1,34 @@
 class Solution {
-
-private: 
+public:
     
-     int t[2501][2501];
+    int t[2501][2501];
 
-     int solve(int idx, int prev, vector<int>& nums, int n){
+    int solve(int i , int prev , int n , vector<int>& nums){
 
-        if(n <= idx){
-            return 0;
+        if(i >= n) return 0;
+
+        if(prev != -1 && t[i][prev] != -1) return t[i][prev];
+
+        int take = 0;
+        if(prev == -1 || nums[prev] < nums[i]){
+            take = 1 + solve(i+1 , i , n , nums);
         }
 
-        if(prev != -1 && t[idx][prev] != -1){
-            return t[idx][prev];
+        int skip = solve(i+1 , prev , n , nums);
+        
+        if(prev != -1){
+            t[i][prev] =  max(take , skip);
         }
         
-        int take = 0;
-        if(prev == -1 || nums[prev] < nums[idx]){
-           take = 1 + solve(idx+1, idx, nums, n);
-        }
-
-        int s = solve(idx+1, prev, nums, n);
-
-        if(prev != -1){
-           return t[idx][prev] = max(take , s);
-        }
-
-        return max(take , s);
-     }
+        return max(take , skip);
+    }
 
 
-public:
+
     int lengthOfLIS(vector<int>& nums) {
+
         int n = nums.size();
-        memset(t, -1, sizeof(t));
-        return solve(0 , -1 , nums , n);
+        memset(t , -1 ,sizeof(t));
+        return solve(0 , -1 , n , nums); 
     }
 };
