cd18f94295b09f60a25b75d6c145c69052ba6790
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2025-12-23 03:02:45 +0530

    Time: 15 ms (5.80%) | Memory: 25.9 MB (9.44%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-
     bool isValidSudoku(vector<vector<char>>& board) {
-
+        
         unordered_set<string> st;
 
         for(int i=0; i<9; i++){
@@ -10,18 +9,18 @@ public:
 
                 if(board[i][j] == '.') continue;
 
-                string row = to_string(board[i][j]) + "_ROW_" + to_string(i);
-                string col = to_string(board[i][j]) + "_COL_" + to_string(j);
-                string box = to_string(board[i][j]) + "_ROW_" + to_string(i/3) + to_string(j/3);
+                string row =  to_string(board[i][j]) + "_ROW_" + to_string(i);
+                string col =  to_string(board[i][j]) + "_COL_" + to_string(j);
+                string box =  to_string(board[i][j]) + "_BOX_" + to_string(i/3) + to_string(j/3);
 
                 if(st.count(row) || st.count(col) || st.count(box)) return false;
 
                 st.insert(row);
                 st.insert(col);
                 st.insert(box);
+                
             }
         }
-        
         return true;
     }
 };
