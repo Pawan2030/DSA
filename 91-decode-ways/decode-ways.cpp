7c7a105197771ed5f569ec0e4e2fe508325687db
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2026-01-09 15:29:50 +0530

    Time: 2 ms (23.82%) | Memory: 8.7 MB (52.31%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -2,36 +2,61 @@ class Solution {
 public:
 
     int n;
-    int dp[101];
+    //int dp[101];
 
-    int solve(int i, string& s){
+    // int solve(int i, string& s){
         
-        if(dp[i] != -1){
-            return dp[i];
+    //     if(dp[i] != -1){
+    //         return dp[i];
+    //     }
+
+    //     if(i==n){
+    //        return 1;
+    //     }
+
+    //     if(s[i] == '0'){
+    //         return dp[i] = 0;
+    //     }
+
+    //     int way = 0;
+
+    //     way += solve(i+1, s);
+
+    //     if(i+1 < n && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')){
+    //         way += solve(i+2 , s);
+    //     }
+
+    //     return dp[i] = way;
+    // }
+
+
+    int tab(string &s){
+
+        vector<int> dp(n+1 , 0);
+
+        dp[n] =  1;
+
+        for(int i=n-1; i>=0; i--){
+
+            if(s[i] == '0'){
+                dp[i] = 0;
+                continue;
+            }
+
+            dp[i] += dp[i+1];
+
+            if(i+1 < n && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')){
+                dp[i] += dp[i+2];
+            }
         }
 
-        if(i==n){
-           return 1;
-        }
-
-        if(s[i] == '0'){
-            return dp[i] = 0;
-        }
-
-        int way = 0;
-
-        way += solve(i+1, s);
-
-        if(i+1 < n && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')){
-            way += solve(i+2 , s);
-        }
-
-        return dp[i] = way;
+        return dp[0];
     }
+
 
     int numDecodings(string s) {
         n = s.size();
-        memset(dp, -1, sizeof(dp));
-        return solve(0 , s);
+        //memset(dp, -1, sizeof(dp));
+        return tab(s);
     }
 };
