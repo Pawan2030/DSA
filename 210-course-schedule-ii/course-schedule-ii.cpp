bde38ce506253adc4d37dc9d41e2150f5f4aaae0
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2026-01-12 16:12:33 +0530

    Time: 5 ms (42.54%) | Memory: 19.3 MB (12.35%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,60 +1,43 @@
 class Solution {
 public:
-    
-    vector<int> topologicalSort(unordered_map<int ,vector<int>> &adj , int n , vector<int> &indegree){
-        
-        //bfs
-        queue<int> q;
-        vector<int> topo;
-        int cnt = 0;
-
-        for(int i=0; i<n; i++){
-            if(indegree[i] == 0){
-                cnt++;
-                q.push(i);
-                topo.push_back(i);
-            }
-        }
-
-        while(!q.empty()){
-
-            int u = q.front();
-            q.pop();
-
-            for(int &v : adj[u]){
-
-                indegree[v]--;
-                if(indegree[v] == 0){
-                    cnt++;
-                    q.push(v);
-                    topo.push_back(v);
-                }
-            }
-        }
-
-        if(cnt == n){
-            return topo;
-        }
-
-        return {};
-    }
-
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         
-        unordered_map<int ,vector<int>> adj;
-        vector<int> indegree(numCourses , 0);
+         unordered_map<int , vector<int>> mp;
+         vector<int> in(numCourses , 0);
 
-        for(auto vec : prerequisites){
-           
-           int a = vec[0];
-           int b = vec[1];
+         for(auto prep : prerequisites){
+              int u = prep[1];
+              int v = prep[0];
+              mp[u].push_back(v);
+              in[v]++;
+         }
 
-           //[a , b]
-           //b -> a
-           indegree[a]++;
-           adj[b].push_back(a);
-        }
+        queue<int> q;
+        vector<int> res; 
 
-        return topologicalSort(adj , numCourses , indegree);
+         for(int i=0; i<in.size(); i++){
+             
+             if(in[i] == 0){
+                q.push(i);
+             }
+         }
+
+         while(!q.empty()){
+
+            int it = q.front();
+            q.pop();
+            res.push_back(it);
+
+            for(int ngr : mp[it]){
+
+                in[ngr]--;
+
+                if(in[ngr] == 0){
+                    q.push(ngr);
+                }
+            }
+         }
+
+         return res.size() == numCourses ? res : vector<int>{};
     }
 };
