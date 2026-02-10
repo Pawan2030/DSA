3edcca0154bb78ea06060e86a9ae776fdc769bfe
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2026-02-10 16:24:28 +0530

    Time: 6 ms (68.79%) | Memory: 13.2 MB (41.99%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,39 +1,69 @@
 class Solution {
 public:
     
-    int solve(int i, int j, string& w1, string& w2, int n1, int n2,  vector<vector<int>>& dp){
+    int n1;
+    int n2;
+
+    int solve(int i, int j, string& w1, string& w2){
         
-        //base case
-        if(j >= n2){
-            return dp[i][j] = n1-i;
+        if(i < 0){
+            return j+1;
+        }
+        
+        if(j < 0){
+            return i+1;
         }
 
-        if(i >= n1){
-            return dp[i][j] = n2-j;
-        }
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        //logic
         if(w1[i] == w2[j]){
-            return dp[i][j] = solve(i+1, j+1, w1, w2, n1, n2, dp);
+            return solve(i-1, j-1, w1, w2);
         }
         else{
-            int insert  = solve(i, j+1, w1, w2, n1, n2, dp);
-            int del     = solve(i+1, j, w1, w2, n1, n2, dp);
-            int replace = solve(i+1, j+1, w1, w2, n1, n2, dp);
 
-            return dp[i][j] = 1+min({insert, del, replace});
+            int insert  = 1+ solve(i,j-1,w1,w2);
+            int remove  = 1+ solve(i-1,j,w1,w2);
+            int replace = 1+ solve(i-1,j-1,w1,w2);
+
+            return min({insert,remove,replace});
         }
+    }
+
+    int solveTab(string& w1, string& w2){
+
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1,0));
+
+        for(int i=0; i<=n1; i++){
+            dp[i][0] = i;
+        }
+
+        for(int i=0; i<=n2; i++){
+            dp[0][i] = i;
+        }
+
+        for(int i=1; i<=n1; i++){
+            for(int j=1; j<=n2; j++){
+
+                if(w1[i-1] == w2[j-1]){
+                   dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int insert  = 1 + dp[i][j-1];
+                    int remove  = 1 + dp[i-1][j];
+                    int replace = 1 + dp[i-1][j-1];
+
+                    dp[i][j] = min({insert,remove,replace});
+                }
+            }
+        }
+
+        return dp[n1][n2];
     }
 
     int minDistance(string word1, string word2) {
         
-        int n1 = word1.size();
-        int n2 = word2.size();
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
-        return solve(0 , 0 , word1, word2, n1, n2, dp);
+        n1 = word1.size();
+        n2 = word2.size();
+
+        return solveTab(word1, word2);
+
     }
 };
