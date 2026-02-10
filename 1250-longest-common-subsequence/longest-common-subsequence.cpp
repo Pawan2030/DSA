4a9bb305e4b1fd8486db0deb72d020691979e470
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2026-02-10 15:40:22 +0530

    Time: 31 ms (38.12%) | Memory: 45.4 MB (6.45%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -4,20 +4,53 @@ public:
     int n1;
     int n2;
 
-    int solve(int i, int j, string& t1, string& t2, vector<vector<int>>& dp){
+    // int solve(int i, int j, string& t1, string& t2, vector<vector<int>>& dp){
 
-        if(i >= n1 || j >= n2) return 0;
+    //     if(i >= n1 || j >= n2) return 0;
+
+    //     if(dp[i][j] != -1){
+    //          return dp[i][j];
+    //     }
+
+    //     if(t1[i] == t2[j]){
+    //         return dp[i][j] = 1 + solve(i+1, j+1, t1 , t2, dp);
+    //     }
+    //     else{
+    //         return dp[i][j] = max(solve(i, j+1, t1 , t2, dp) ,  solve(i+1, j, t1 , t2, dp));
+    //     }
+    // }
+
+    int solveR(int i, int j, string& t1, string& t2, vector<vector<int>>& dp){
+
+        if(i < 0 || j < 0) return 0;
 
         if(dp[i][j] != -1){
              return dp[i][j];
         }
 
         if(t1[i] == t2[j]){
-            return dp[i][j] = 1 + solve(i+1, j+1, t1 , t2, dp);
+            return dp[i][j] = 1 + solveR(i-1, j-1, t1 , t2, dp);
         }
         else{
-            return dp[i][j] = max(solve(i, j+1, t1 , t2, dp) ,  solve(i+1, j, t1 , t2, dp));
+            return dp[i][j] = max(solveR(i, j-1, t1 , t2, dp) ,  solveR(i-1, j, t1 , t2, dp));
         }
+    }
+
+    int solveTab(string& t1, string& t2){
+       
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1 , 0));
+
+        for(int i=n1-1; i>=0; i--){
+            for(int j=n2-1; j>=0; j--){
+                if(t1[i] == t2[j]){
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j] = max(dp[i][j+1] ,  dp[i+1][j]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 
     int longestCommonSubsequence(string text1, string text2) {
@@ -25,6 +58,6 @@ public:
         n1 = text1.size();
         n2 = text2.size();
         vector<vector<int>> dp(n1+1, vector<int>(n2+1 , -1));
-        return solve(0,0,text1,text2,dp);
+        return solveTab(text1,text2);
     }
 };
