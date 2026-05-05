d26ef2446d816328ff94b164a0472807a369adf6
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2026-05-05 16:35:20 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (94.62%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -10,52 +10,33 @@
  */
 class Solution {
 public:
-    
-    int sizeOfList(ListNode* head){
-
-        ListNode* temp = head;
-        int cnt = 0;
-
-        while(temp){
-            temp = temp->next;
-            cnt += 1;
-        }
-
-        return cnt;
-    }
-
     ListNode* rotateRight(ListNode* head, int k) {
         
-        // head is null
-        if(head == NULL || k == 0) return head;
-        //if k == size of linklist return head
-        int n = sizeOfList(head);
-        if(n == k) return head;
 
-        //if rotation k > n
-        k = k % n;
+        if(!head) return NULL;
+        
+        int n = 1;
+        ListNode* tail = head;
 
-        if(k == 0) return head;
+        while(tail->next != NULL){
+            tail = tail->next;
+            n++;
+        }
+
+        tail->next = head;
+        n = n-(k%n);
 
         int cnt = 1;
-        int num = n-k;
-        ListNode* temp = head;
 
-        while(cnt < num){
-           temp = temp->next;
-           cnt += 1;
+
+        while(cnt < n){
+           head = head->next;
+           cnt++;
         }
 
-        ListNode* newHead = temp->next;
-        temp->next = NULL;
-
-        ListNode* t = newHead;
-
-        while(t->next != NULL){
-            t = t->next;
-        }
-
-        t->next = head;
+        ListNode* newHead = head->next;
+        head->next = NULL;
         return newHead;
+
     }
 };
