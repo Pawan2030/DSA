ba214ae57de6804027f373fd01e8446b4b604c8d
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2026-05-10 03:00:32 +0530

    Time: 12 ms (72.99%) | Memory: 17.8 MB (51.82%) - LeetSync

---
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+    
+    void rotateArr(vector<int> layerEle , int len, int k){
+        
+        vector<int> temp(len);
+    
+        for(int i=0; i<len; i++){
+          temp[i] = layerEle[(i+k)%len];
+        }
+
+        layerEle = temp;
+    }
+
+    vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k){
+        
+        int m = grid.size();
+        int n = grid[0].size();
+        int layer = min(m/2 , n/2);
+
+        for(int l=0; l<layer; l++){
+            
+            vector<int> layerEle;
+            int top = l;
+            int bottom = m-l-1;
+            int left = l;
+            int right = n-l-1;
+            
+            // top-row L-R
+            for(int i=left; i<=right; i++){
+               layerEle.push_back(grid[top][i]); 
+            }
+
+            //right-col T->D
+            for(int i=top+1; i<=bottom-1; i++){
+              layerEle.push_back(grid[i][right]); 
+            }
+
+            //bottom R->L
+            for(int i=right; i>=left; i--){
+              layerEle.push_back(grid[bottom][i]); 
+            }
+
+            //B->T
+            for(int i=bottom-1; i>=top+1; i--){
+               layerEle.push_back(grid[i][left]); 
+            }
+
+            int len = layerEle.size();
+            int rotateK  = k % len;
+
+            //rotateArr(layerEle,len,k);
+            rotate(begin(layerEle) , begin(layerEle)+rotateK , end(layerEle));
+
+            int idx = 0;
+
+            // top-row L-R
+            for(int i=left; i<=right; i++){
+               grid[top][i] = layerEle[idx++];
+            }
+
+            //right-col T->D
+            for(int i=top+1; i<=bottom-1; i++){
+               grid[i][right] = layerEle[idx++];
+            }
+
+            //bottom R->L
+            for(int i=right; i>=left; i--){
+               grid[bottom][i] = layerEle[idx++];
+            }
+
+            //B->T
+            for(int i=bottom-1; i>=top+1; i--){
+               grid[i][left] = layerEle[idx++];
+            }
+
+        }
+        return grid;
+    }
+};
