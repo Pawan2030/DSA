dae5fc21b0b041add20612ba02b05bbfc7243644
Pawan Mehta <136910101+Pawan2030@users.noreply.github.com>
2026-06-09 01:20:01 +0530

    Time: 15 ms (11.60%) | Memory: 139.2 MB (21.27%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
-
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-
+        
         int n = nums.size();
-        vector<int> res;
+        queue<int> q1;
+        queue<int> q2;
+        int count = 0;
 
         for(int i=0; i<n; i++){
-            if(nums[i] < pivot){
-                res.push_back(nums[i]);
-            }
-        }
 
-        for(int i=0; i<n; i++){
-            if(nums[i] == pivot){
-                res.push_back(nums[i]);
-            }
-        }
-
-        for(int i=0; i<n; i++){
             if(nums[i] > pivot){
-                res.push_back(nums[i]);
+                q1.push(nums[i]);
+            }
+            else if(nums[i] < pivot){
+                q2.push(nums[i]);
+            }
+            else{
+                count++;
             }
         }
 
-        return res;
+        while(count--){
+            q2.push(pivot);
+        }
+        
+        vector<int> ans;
+
+        while(!q2.empty()){
+           ans.push_back(q2.front());
+           q2.pop();
+        }
+
+        while(!q1.empty()){
+           ans.push_back(q1.front());
+           q1.pop();
+        }
+
+        return ans;
     }
 };
